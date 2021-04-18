d8daa8393936db837501ef36bec1698aceba3ad9
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-04-18 00:15:23 -0300

    add comando condicional

--- q7.sh
+++ q7.sh
@@ -1,8 +1,15 @@
-#!bin/bash
+#!/bin/bash
 echo 'Observando os arquivos mais recentes que tem o mesmo nome'
 #procura p arquivos que tem o mesmo nome e classifica por ordem de criacao
 find . -name 'relatorio' -exec ls -l '{}' \;
 echo -e '\nConferindo se os arquivos sao iguais'
 #confere se os dois arquivos que tem o mesmo nome sao iguais
-find . -name 'relatorio' -exec ls -l '{}' \;awk '{print $9}'|xargs diff
+verifica=`find . -name 'relatorio' -exec ls -l '{}' \;|awk '{print $9}'|xargs diff`
 
+if [ -z "$verifica" ]
+then
+    echo 'Arquivos sao iguais'
+else
+    echo -e 'Arquivos sao diferentes\n'
+    echo $verifica
+fi
