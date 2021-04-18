4a003589be66c5a23084771bde1833ae5511af61
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-04-18 00:26:34 -0300

    add comando para verificar arquivo mais recente

--- q7.sh
+++ q7.sh
@@ -2,6 +2,12 @@
 echo 'Observando os arquivos mais recentes que tem o mesmo nome'
 #procura p arquivos que tem o mesmo nome e classifica por ordem de criacao
 find . -name 'relatorio' -exec ls -l '{}' \;
+
+#Exibe o arquivo mais recente entre os duplicados
+echo -e '\nO arquivo mais recente eh o:'
+find . -name 'relatorio' -exec ls -l '{}' \;|sort -r|head -n 1
+
+
 echo -e '\nConferindo se os arquivos sao iguais'
 #confere se os dois arquivos que tem o mesmo nome sao iguais
 verifica=`find . -name 'relatorio' -exec ls -l '{}' \;|awk '{print $9}'|xargs diff`
