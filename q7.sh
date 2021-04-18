e377b806fb1dd430526d13e53e3f396e27ba17c0
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-04-17 23:57:18 -0300

    upload script

--- q7.sh
+++ q7.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 echo 'Observando os arquivos mais recentes que tem o mesmo nome'
 #procura p arquivos que tem o mesmo nome e classifica por ordem de criacao
-find . -name 'relatorio duplicado' -exec ls -l '{}' \;
+find . -name 'relatorio' -exec ls -l '{}' \;
 echo -e '\nConferindo se os arquivos sao iguais'
 #confere se os dois arquivos que tem o mesmo nome sao iguais
-find . -name 'relatorio duplicado' -exec ls -l '{}' \;awk '{print $9}'|xargs diff
+find . -name 'relatorio' -exec ls -l '{}' \;awk '{print $9}'|xargs diff
 
