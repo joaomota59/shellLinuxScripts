7bb0a5b3d66d0098c902d621619e442122a9aa31
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-04-20 02:04:01 -0300

    usuarios e diretorios home correspondentes

--- q-4.sh
+++ q-4.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-info=`cat /etc/passwd` #pega todas informacoes referentes aos usuarios
+#usuarios e seus diretorios home
 echo -e 'Usuarios => Diretorio home\n'
-for i in $info
-do
-    cat /etc/passwd | awk -F: '{print $1 "=>" $6}'
-done
+cat /etc/passwd | awk -F: '{print $1 "=>" $6}'
