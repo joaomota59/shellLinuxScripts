6d5bfee7d830ad79cffbf341da6def267643f790
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-04-21 21:22:18 -0300

    palavra contida na outra - verificacao de parametros

--- q-6.sh
+++ q-6.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+if [ -z "$1" ] #se o usuario nao entrar com nenhum parametro..
+then
+   echo 'Entre com dois parametros!'
+   exit 0 #finaliza o programa, programa executado com sucesso
+fi
+
+if [ -z "$2" ] #se o usuario nao entrar com o segundo parametro..
+then
+   echo 'Entre com o segundo parametro!'
+   exit 0 #finaliza o programa, programa executado com sucesso
+fi
+
 k=`echo $2 | grep $1` #verifica se a primeira palavra esta contida na segunda
 
 if [ ! -z $k ] #se nao for vazio
