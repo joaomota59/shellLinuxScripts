99db824f187c94457ecc72b23a70e7c5ab695654
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-04-26 12:19:54 -0300

    processos que geram mais de 5 processos filhos

--- q-7.sh
+++ q-7.sh
@@ -1,21 +1,30 @@
 #!/bin/bash
 #script que informa quais processos tem mais de 5 processos filhos
 
-while true
-do
-salvaProcesso=`ps -eo pid,ppid,comm | tail -n +2 > processos` #salva as informacoes do processo em um txt, desconsiderando a primeira linha(cabecalho)
+
+echo 'Aperte crtl + c para parar o script, apos iniciar!'
+read -p '1-Iniciar 2-Sair: ' entrada
+if [ $entrada == 1 ]
+then
+
+	while true
+	do
+		salvaProcesso=`ps -eo pid,ppid,comm | tail -n +2 > processos` #salva as informacoes do processo em um txt, desconsiderando a primeira linha(cabecalho)
 
 
-cat processos | sort -k2 -n | awk '{print $2}' | uniq -c | 
-awk '{
+		cat processos | sort -k2 -n | awk '{print $2}' | uniq -c | 
+		awk '{
 
-if ($1>5){
-print "Processo de PID "$2" criou "$1" processos filhos: "
-system("pgrep -P" $2)
+		if ($1>5){
+		print "Processo de PID "$2" criou "$1" processos filhos: "
+		system("pgrep -P" $2)
 
-}   
-}'
-sleep 5 #time sleep em segundos
-clear #limpa a tela
-done
+		}   
+		}'
+		sleep 5 #time sleep em segundos
+		clear #limpa a tela
+	done
 
+else
+    echo 'Algoritmo finalizado!'
+fi
