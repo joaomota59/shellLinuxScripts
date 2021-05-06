d59b2ec40a39f26f2261f3310b930a09e9752d3c
João Lucas Mota <54906718+joaomota59@users.noreply.github.com>
2021-05-05 21:09:26 -0300

    econtra arquivos que possuem o mesmo nome e diz qual eh o mais recente

--- q7.sh
+++ q7.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+
 echo 'Observando os arquivos mais recentes que tem o mesmo nome'
 #procura p arquivos que tem o mesmo nome e classifica por ordem de criacao
-find . -name 'relatorio' -exec ls -l '{}' \;
+sudo find /home/aluno -name 'relatorio' -exec ls -l '{}' \;
 
 #Exibe o arquivo mais recente entre os duplicados
 echo -e '\nO arquivo mais recente eh o:'
-find . -name 'relatorio' -exec ls -l '{}' \;|sort -r|head -n 1
+sudo find /home/aluno -name 'relatorio' -exec ls -l '{}' \;|sort -r|head -n 1
 
 
 echo -e '\nConferindo se os arquivos sao iguais'
 #confere se os dois arquivos que tem o mesmo nome sao iguais
-verifica=`find . -name 'relatorio' -exec ls -l '{}' \;|awk '{print $9}'|xargs diff`
+verifica=`sudo find /home/aluno -name 'relatorio' -exec ls -l '{}' \;|awk '{print $9}'|xargs diff`
 
 if [ -z "$verifica" ]
 then
